f84fc0a43061c70f9b24b816b783869d5f0217a4
chandkiransingh <rs695013@gmail.com>
2022-08-16 02:39:13 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const indexRoute = require("./routes");
+const rateLimit = require("express-rate-limit");
+const app = express();
+const port = 3000;
+
+app.use(
+  rateLimit({
+    windowMs: 12 * 60 * 60 * 1000, // 12 hour duration in milliseconds
+    max: 6,
+    message: "You exceeded 100 requests in 12 hour limit!",
+    headers: true,
+  })
+);
+
+app.use("/posts", indexRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
